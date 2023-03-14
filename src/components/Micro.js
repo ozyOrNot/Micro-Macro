0bf3da6192f6372e59decfffa667585588304eb8
Vlad Rah <rahmadjanov.of@gmail.com>
2023-03-14 17:49:19 +0300

    white page

--- src/components/Micro.js
+++ src/components/Micro.js
@@ -1,30 +1,10 @@
-import React, { useRef } from "react";
+import React from "react";
 
 const Micro = () => {
-  const microRef = useRef();
-
-  function handleDown(fieldX, fieldY) {
-    microRef.current.style.position = "absolute";
-    moveAt(fieldX, fieldY)
-    document.body.appendChild(microRef.current)
-    microRef.current.style.zIndex = 1000;
-  }
-
-  function moveAt(fieldX, fieldY) {
-    microRef.current.style.left = fieldX - microRef.current.offsetWidth + "px";
-    microRef.current.style.top = fieldY - microRef.current.offsetHeight + "px";
-  }
-
-  document.onmousemove = function(e){
-    moveAt(e);
-  }
-
   return (
     <img
-      ref="microRef"
-      onMouseDown={handleDown}
       src={require("../img/ball.jpg")}
-      alt=""
+      alt="micro"
       width={50}
       height={50}
     />
